6ded7453e8a7db43947d901406db1c60f854c2f2
Cecilia Wong <ceciliakhwong@gmail.com>
2017-10-10 17:18:45 -0400

    Define create method to add new picture to database, if picture saves redirect to index, if not render new.html.erb

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -12,6 +12,19 @@ class PicturesController < ApplicationController
   end
 
   def create
-    render text: "Received POST request to 'pictures' with the data URL: #{ params.inspect }"
+    @picture = Picture.new
+
+    @picture.title = params[:picture][:title]
+    @picture.artist = params[:picture][:artist]
+    @picture.url = params[:picture][:url]
+
+    if @picture.save
+      # if the picture gets saved, generate a get request to "/pictures"
+      redirect_to '/pictures'
+    else
+      # otherwise render new.html.erb
+      rander :new
+    end
   end
+
 end
