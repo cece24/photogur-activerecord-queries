88de31b90d7b4bfa94f8e715b1390e9e05fa6ac9
Cecilia Wong <ceciliakhwong@gmail.com>
2017-10-16 17:42:45 -0400

    Add validation for url, must begin with 'http'

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -3,4 +3,5 @@ class Picture < ApplicationRecord
   validates :title, length: { in: 3..20 }
   validates :url, presence: true
   validates :url, uniqueness: true
+  validates :url, format: { with: /\Ahttp/, message: "must start with 'http'" }
 end
